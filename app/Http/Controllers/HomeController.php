e28dcba261598c5097e9d177f444653f336a82a0
gita02 <cahyagita2@gmail.com>
2019-02-20 07:40:50 +0700

    firstedit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function edit_profile(Request $data)
+    {
+        $user = User::find(Auth::id());
+        $user->update([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+        return redirect('home');
+    }
+
+    public function edit()
+    {
+        return view('formprofile');
+    }
+}
